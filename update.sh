615eaecd44989cf9380857abe022b985890ff38c
OpacApp team <info@opacapp.nde>
2016-10-19 19:08:39 +0200

    Improved git commands

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@ git checkout master .
 git reset --hard
 git pull -f origin master
 python3 fetch.py
-git diff --exit-code bibs && exit 0
+git diff --exit-code HEAD bibs && exit 0
 git add -A bibs/*.json last_library_config_update.txt
 git commit -m "Data update" --author="OpacApp team <info@opacapp.de>" bibs/*.json last_library_config_update.txt
 git push origin master
