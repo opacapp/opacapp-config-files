20498287fc4bf4cc22657bd09347ebfd89d36284
Raphael Michel <mail@raphaelmichel.de>
2019-12-12 12:33:55 +0100

    Update update

--- update.sh
+++ update.sh
@@ -2,6 +2,7 @@
 git checkout master .
 git reset --hard
 git pull -f origin master
+git rm bibs/*.json
 python3 fetch.py
 git diff --exit-code HEAD bibs && exit 0
 git add -A bibs/*.json last_library_config_update.txt
