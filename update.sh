b9015085c40bf55846be8e176835e5394c72958f
Raphael Michel <mail@raphaelmichel.de>
2019-12-12 12:36:34 +0100

    Do not use git rm

--- update.sh
+++ update.sh
@@ -2,9 +2,10 @@
 git checkout master .
 git reset --hard
 git pull -f origin master
-git rm bibs/*.json
+rm bibs/*.json
 python3 fetch.py
 git diff --exit-code HEAD bibs && exit 0
+git add -u
 git add -A bibs/*.json last_library_config_update.txt
 git commit -m "Data update" --author="OpacApp team <info@opacapp.de>" bibs/*.json last_library_config_update.txt
 git push origin master
