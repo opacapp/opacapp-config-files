79d80fb2d61c44f27338dc316383eb2c97a4ac08
OpacApp team <info@opacapp.nde>
2019-12-12 12:38:15 +0100

    resort to add -A

--- update.sh
+++ update.sh
@@ -5,8 +5,7 @@ git pull -f origin master
 rm bibs/*.json
 python3 fetch.py
 git diff --exit-code HEAD bibs && exit 0
-git add -u
-git add -A bibs/*.json last_library_config_update.txt
+git add -A
 git commit -m "Data update" --author="OpacApp team <info@opacapp.de>" bibs/*.json last_library_config_update.txt
 git push origin master
 
