8a4d9d67242129e4b9e3b1f1b0b98e83dd63fb4c
OpacApp team <info@opacapp.nde>
2019-12-12 12:39:04 +0100

    aCommit all changes

--- update.sh
+++ update.sh
@@ -6,6 +6,6 @@ rm bibs/*.json
 python3 fetch.py
 git diff --exit-code HEAD bibs && exit 0
 git add -A
-git commit -m "Data update" --author="OpacApp team <info@opacapp.de>" bibs/*.json last_library_config_update.txt
+git commit -m "Data update" --author="OpacApp team <info@opacapp.de>"
 git push origin master
 
