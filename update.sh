147dec50ea5da3d90e47c73921b314326f569901
OpacApp team <info@opacapp.nde>
2023-01-18 17:23:19 +0100

    Update update script

--- update.sh
+++ update.sh
@@ -5,7 +5,7 @@ git pull -f origin master
 rm bibs/*.json
 python3 fetch.py
 git add -A
-git diff --cached --exit-code HEAD bibs && exit 0
+git diff --cached --exit-code HEAD bibs && git reset HEAD . && git checkout . && exit 0
 git commit -m "Data update" --author="OpacApp team <info@opacapp.de>"
 git push origin master
 
